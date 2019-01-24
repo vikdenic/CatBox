40ae5ccb0342e21fa6d0f8bfa0b321811e2d2f76
Vik Denic <vik.denic@gmail.com>
2019-01-24 15:50:02 -0600

    Handle likes via cloud functions

---
+++ functions/index.js
@@ -0,0 +1,48 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
+
+exports.onLikeCat = functions.firestore
+  .document('/likes/{likeId}')
+  .onCreate(event => {
+    let catId, userId;
+    [catId, userId] = event.params.likeId.split(':');
+
+    const db = admin.firestore();
+    const catRef = db.collection('cats').doc(catId);
+    db.runTransaction(t => {
+      return t.get(catRef)
+        .then(doc => {
+          t.update(catRef, {
+            like_counter: (doc.data().like_counter || 0) + 1
+          });
+        })
+    }).then(result => {
+      console.log('⬆ Increased aggregate cat like counter.');
+    }).catch(err => {
+      console.log('🛑 Failed to increase aggregate cat like counter.', err);
+    });
+  })
+
+exports.onLikeCat = functions.firestore
+    .document('/likes/{likeId}')
+    .onDelete(event => {
+      let catId, userId;
+      [catId, userId] = event.params.likeId.split(':');
+
+      const db = admin.firestore();
+      const catRef = db.collection('cats').doc(catId);
+      db.runTransaction(t => {
+        return t.get(catRef)
+          .then(doc => {
+            t.update(catRef, {
+              like_counter: (doc.data().like_counter || 0) - 1
+            });
+          })
+      }).then(result => {
+        console.log('⬇️ Decreased aggregate cat like counter.');
+      }).catch(err => {
+        console.log('🛑 Failed to decrease aggregate cat like counter.', err);
+      });
+    })
--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,7 +1,7 @@
-import 'dart:convert';
+import 'dart:async';
+
 import 'package:catbox/model/cat.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -19,23 +19,57 @@ class CatAPI {
     GoogleSignInAccount googleUser = await _googleSignIn.signIn();
     GoogleSignInAuthentication googleAuth = await googleUser.authentication;
     FirebaseUser user = await _auth.signInWithGoogle(
-        accessToken: googleAuth.accessToken,
-        idToken: googleAuth.idToken,
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
     );
     assert(user.email != null);
     assert(user.displayName != null);
     assert(!user.isAnonymous);
     assert(await user.getIdToken() != null);
+
     FirebaseUser currentUser = await _auth.currentUser();
     assert(user.uid == currentUser.uid);
 
     return new CatAPI(user);
   }
 
-  static List<Cat> allCatsFromJson(String jsonData) {
-    List<Cat> cats = [];
-    json.decode(jsonData)['cats'].forEach((cat) => cats.add(_fromMap(cat)));
-    return cats;
+  Cat _fromDocumentSnapshot(DocumentSnapshot snapshot) {
+    final data = snapshot.data;
+
+    return new Cat(
+        documentId: snapshot.documentID,
+        externalId: data['id'],
+        name: data['name'],
+        description: data['description'],
+        avatarUrl: data['image_url'],
+        location: data['location'],
+        likeCounter: data['like_counter'],
+        isAdopted: data['adopted'],
+        pictures: new List<String>.from(data['pictures']),
+        cattributes: new List<String>.from(data['cattributes']));
+  }
+
+  Future likeCat(Cat cat) async {
+    await Firestore.instance
+        .collection('likes')
+        .document('${cat.documentId}:${this.firebaseUser.uid}')
+        .setData({});
+  }
+
+  Future unlikeCat(Cat cat) async {
+    await Firestore.instance
+        .collection('likes')
+        .document('${cat.documentId}:${this.firebaseUser.uid}')
+        .delete();
+  }
+
+  Future<bool> hasLikedCat(Cat cat) async {
+    final like = await Firestore.instance
+        .collection('likes')
+        .document('${cat.documentId}:${this.firebaseUser.uid}')
+        .get();
+
+    return like.exists;
   }
 
   Future<List<Cat>> getAllCats() async {
@@ -50,38 +84,6 @@ class CatAPI {
         .collection('cats')
         .document(cat.documentId)
         .snapshots()
-        .listen((snapshot) => onChange(_fromDocumentSnapshot(snapshot))
-    );
-  }
-
-  Cat _fromDocumentSnapshot(DocumentSnapshot snapshot) {
-    final data = snapshot.data;
-
-    return new Cat(
-      documentId: snapshot.documentID,
-      externalId: data['id'],
-      name: data['name'],
-      description: data['description'],
-      avatarUrl: data['image_url'],
-      location: data['location'],
-      likeCounter: data['like_counter'],
-      isAdopted: data['adopted'],
-      pictures: new List<String>.from(data['pictures']),
-      cattributes: new List<String>.from(data['cattributes']),
-    );
-  }
-
-  static Cat _fromMap(Map<String, dynamic> map) {
-    return new Cat(
-      externalId: map['id'],
-      name: map['name'],
-      description: map['description'],
-      avatarUrl: map['image_url'],
-      location: map['location'],
-      likeCounter: map['like_counter'],
-      isAdopted: map['adopted'],
-      pictures: new List<String>.from(map['pictures']),
-      cattributes: new List<String>.from(map['cattributes']),
-    );
+        .listen((snapshot) => onChange(_fromDocumentSnapshot(snapshot)));
   }
 }
